adb6b1fe9bc299568fd6c14f67a7a50e2204fd6e
nehaa20601 <62811492+nehaa20601@users.noreply.github.com>
2020-05-03 01:48:46 -0400

    cleaned login/sign up screens

--- lib/screens/log_in.dart
+++ lib/screens/log_in.dart
@@ -1,36 +1,147 @@
 import 'package:flutter/material.dart';
 import '../theme.dart';
 
-class LogIn extends StatelessWidget {
+const FONT_SIZE = 20.0;
+class LogIn extends StatefulWidget {
   @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LogIn> {
+
+  bool _rememberMe = false; //state of remember me checkbox
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
+        body: Container(
+          height: double.infinity,
+          child: SingleChildScrollView(
+            physics: AlwaysScrollableScrollPhysics(),
+            padding: EdgeInsets.symmetric(
+              horizontal: 40.0,
+              vertical: 120.0,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Sign In',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                _buildTextField("Email", false),
+                _buildTextField("Password", true),
+                SizedBox(height: 20.0),
+                _buildRememberMe(),
+                _buildLogInButton(),
+                _buildSignUpButton(),
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+
+  Widget _buildTextField(String text, bool passwordField) {
+    return Container(
+      child: TextField(
+        decoration: InputDecoration(
+            labelText: text,
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Theme.of(context).primaryColor)
+            ),
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: AppColors.accentDark)
+            )
+        ),
+        obscureText: passwordField,
+        style: TextStyle(
+          color: AppColors.primary,
+          fontSize: 16.0,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRememberMe() {
+    return Container(
+      child: Row(
         children: <Widget>[
-          AppTextField(
-            hintText: "Username",
+          Theme(
+            data: ThemeData(unselectedWidgetColor: AppColors.primary),
+            child: Checkbox(
+              value: _rememberMe,
+              checkColor: AppColors.accentDark,
+              activeColor: AppColors.accent,
+              onChanged: (value) {
+                setState(() {
+                  _rememberMe = value;
+                });
+              },
+            ),
           ),
-          AppTextField(
-            hintText: "Password",
-          ),
-          Center(
-              child: AppButton(
-                text: "Log in",
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, "/map_explore");
-                },
-              )
-          ),
-          Center(
-              child: AppButton(
-                text: "Sign Up",
-                onPressed: () {
-                  Navigator.pushNamed(context, "/log_in/sign_up");
-                },
-              )
+          Text(
+            'Remember Me',
+            style: TextStyle(
+              color: AppColors.primary,
+              fontSize: 14.0,
+            ),
           ),
         ],
       ),
     );
   }
+
+  Widget _buildLogInButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () => Navigator.pushReplacementNamed(context, "/map_explore"),
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Theme.of(context).buttonColor,
+        child: Text(
+          'LOGIN',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignUpButton() {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, "/log_in/sign_up"),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'Don\'t have an account? ',
+              style: TextStyle(
+                color: AppColors.primary,
+                fontSize: 14.0,
+              ),
+            ),
+            TextSpan(
+              text: 'Sign Up',
+              style: TextStyle(
+                color: AppColors.primary,
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
 }
--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -1,36 +1,110 @@
 import 'package:flutter/material.dart';
 import '../theme.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+class _SignUpScreenState extends State<SignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: <Widget>[
-          AppTextField(
-            hintText: "First Name",
+      body: Container(
+        height: double.infinity,
+        child: SingleChildScrollView(
+          physics: AlwaysScrollableScrollPhysics(),
+          padding: EdgeInsets.symmetric(
+            horizontal: 40.0,
+            vertical: 120.0,
           ),
-          AppTextField(
-            hintText: "Last Name",
-          ),
-          AppTextField(
-            hintText: "Email",
-          ),
-          AppTextField(
-            hintText: "Password",
-          ),
-          AppTextField(
-            hintText: "Confirm Password",
-          ),
-          Center(
-              child: AppButton(
-                text: "Sign Up",
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              )
-          ),
-        ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Sign Up",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              _buildTextField("First Name", false),
+              _buildTextField("Last Name", false),
+              _buildTextField("Email", false),
+              _buildTextField("Password", true),
+              _buildTextField("Confirm Password", true),
+              _buildSignUpButton(),
+              _buildLogInButton(),
+            ],
+          )
+        )
+      )
+    );
+  }
+
+  Widget _buildTextField(String text, bool passwordField) {
+    return Container(
+      child: TextField(
+        decoration: InputDecoration(
+            labelText: text,
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Theme.of(context).primaryColor)
+            ),
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: AppColors.accentDark)
+            )
+        ),
+        obscureText: passwordField,
+        style: TextStyle(
+          color: AppColors.primary,
+          fontSize: 16.0,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSignUpButton() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () => Navigator.pushNamed(context, "/log_in"),
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Theme.of(context).buttonColor,
+        child: Text(
+          'SIGN UP',
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLogInButton() {
+    return GestureDetector(
+      onTap: () => Navigator.pushNamed(context, "/log_in"),
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: 'Already have an account? ',
+              style: TextStyle(
+                color: AppColors.primary,
+                fontSize: 14.0,
+              ),
+            ),
+            TextSpan(
+              text: 'Login',
+              style: TextStyle(
+                color: AppColors.primary,
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
